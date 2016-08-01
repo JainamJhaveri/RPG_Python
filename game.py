3b78dffa759f92ff8514607f2fe302243f80f2ad
Bloopomaniac <13bce024@nirmauni.ac.in>
2016-08-01 20:45:03 +0530

    Add files via upload
    Added level up functionality, and adaptive strength for enemies..

--- game.py
+++ game.py
@@ -5,15 +5,28 @@ import random
 clear = lambda: os.system('clear')  # on Linux System
 weapons = {"Great Sword":40}
 
+#Changes Made By Dhaval:
+#Level Up Functionality For Player (Includes exp, maxexp and level data members for player
+#Adaptive strength(health and attack) for enemies
+#Adaptive gain(gold and exp) from enemies
+
+#To Do Ideas:
+#Save and Load - File Handling - Simply Read and Write Player Stats
+#Storyline? Boss Enemy?
+#Dialogue? Humor?
+#Text Graphic Display?
+
 class Player:
     maxhealth = 100
-
+    level = 1
+    maxexp = 100
     def __init__(self, name):
         self.name = name
         self.health = self.maxhealth
+        self.exp = 0
         self.base_attack = 10
         self.gold = 40
-        self.potions = 0
+        self.potions = 2
         self.weap = ["Rusty Sword"]
         self.curweap = ["Rusty Sword"]
 
@@ -27,24 +40,36 @@ class Player:
 
         return attack
 
-class Goblin:
-    maxhealth = 50
+    #@property
+    def expgain(self,gain):
+        self.exp += gain
+        if(self.exp>=self.maxexp):
+            self.level += 1
+            self.base_attack = int(self.base_attack * 1.2)
+            self.maxhealth = int(self.maxhealth * 1.2)
+            self.exp -= self.maxexp
+            self.maxexp *= 2
+            self.health = self.maxhealth
+            print("Congratulations! You Have Levelled Up!")
 
-    def __init__(self, name):
+class Goblin:
+
+    def __init__(self, name, maxh):
         self.name = name
+        self.maxhealth = maxh*0.5
         self.health = self.maxhealth
-        self.attack = 5
-        self.goldgain = 10
+        self.attack = int(self.maxhealth/10)
+        self.goldgain = int(self.maxhealth/5)
 
 
 class Zombie:
-    maxhealth = 70
 
-    def __init__(self, name):
+    def __init__(self, name, maxh):
         self.name = name
+        self.maxhealth = maxh*0.7
         self.health = self.maxhealth
-        self.attack = 7
-        self.goldgain = 15
+        self.attack = int(self.maxhealth/10)
+        self.goldgain = int(self.maxhealth/5)
 
 
 def store():
@@ -52,7 +77,8 @@ def store():
     print("Welcome to the shop")
     print("What would you like to buy?")
     print("1. Great Sword")
-    print("2. Back")
+    print("2. Potion")
+    print("3. Back")
     option = input('-->')
 
     if(option in weapons):
@@ -62,12 +88,17 @@ def store():
             print("You have bought %s" %option)
         else:
             print("You don't have enough gold")
-    elif(option == "Back"):
+    elif(option == "2"):
+        if(PlayerIG.gold >= 20):
+            PlayerIG.gold -= 20
+            PlayerIG.potions += 1
+            print("You have successfully bought a potion! You now have %i potions" %PlayerIG.potions)
+    elif(option == "3"):
         start1()
     else:
         print("That item doesn't exit")
 
-    input('')
+    input('\nPress Return To Continue')
     store()
 
 
@@ -75,9 +106,9 @@ def prefight():
     enemynum = random.randint(1, 2)
     global enemy
     if (enemynum == 1):
-        enemy = GoblinIG
+        enemy = Goblin("Goblin",PlayerIG.maxhealth)
     else:
-        enemy = ZombieIG
+        enemy = Zombie("Zombie",PlayerIG.maxhealth)
     fight()
 
 
@@ -88,10 +119,12 @@ def dead():
 
 def win():
     PlayerIG.gold += enemy.goldgain
-    enemy.health = enemy.health = enemy.maxhealth
+    #enemy.health = enemy.health = enemy.maxhealth
     print("You have defeated the %s" % enemy.name)
-    print("You found %i gold" % enemy.goldgain)
-    input('')
+    print("You have found %i gold" % enemy.goldgain)
+    print("You have gained %i experience" % int(enemy.maxhealth/3))
+    PlayerIG.expgain(int(enemy.maxhealth / 3))
+    input('Press Return To Continue')
     start1()
 
 
@@ -104,18 +137,19 @@ def attack():
     else:
         enemy.health -= PAttack
         print("You did %i damage" % PAttack)
-    input(' ')
-    # clear()
-    if (EAttack == EAttack / 2):
-        print("Enemy missed")
+    if(enemy.health<=0):
+        win()
     else:
-        PlayerIG.health -= EAttack
-        print("The enemy did %i damage" % EAttack)
-    input(' ')
-    if (PlayerIG.health <= 0):
-        dead()
-    else:
-        fight()
+        # clear()
+        if (EAttack == EAttack / 2):
+            print("Enemy missed")
+        else:
+            PlayerIG.health -= EAttack
+            print("The enemy did %i damage" % EAttack)
+        if (PlayerIG.health <= 0):
+            dead()
+        else:
+            fight()
 
 
 def drinkpotion():
@@ -124,6 +158,7 @@ def drinkpotion():
     if (PlayerIG.potions == 0):
         print("You don't have any potions")
     else:
+        PlayerIG.potions -= 1
         PlayerIG.health += 50
         if (PlayerIG.health > PlayerIG.maxhealth):
             PlayerIG.health = PlayerIG.maxhealth
@@ -177,8 +212,10 @@ def fight():
 def start1():
     # clear()
     print("Name: %s" % (PlayerIG.name))
+    print("Level: %s" % (PlayerIG.level))
     print("Attack: %i" % (PlayerIG.attack))
     print("Health: %i/%i" % (PlayerIG.health, PlayerIG.maxhealth))
+    print("Experience Points: %i/%i" % (PlayerIG.exp, PlayerIG.maxexp))
     print("Current Weapon: %s" %PlayerIG.curweap)
     print("Gold: %i" % (PlayerIG.gold))
     print("Potions: %i" % (PlayerIG.potions))
@@ -226,6 +263,6 @@ def main():
         main()
 
 
-GoblinIG = Goblin('Goblin')
-ZombieIG = Zombie('Zombie')
+#GoblinIG = Goblin('Goblin')
+#ZombieIG = Zombie('Zombie')
 main()
